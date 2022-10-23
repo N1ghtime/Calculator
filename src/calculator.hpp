cd265196225de37e792fd78dafbe249f9b23c901
ebnurmi <ebnurmi@gmail.com>
2022-10-23 21:46:54 +0300

    add png and minor modifications

---
+++ src/calculator.hpp
@@ -0,0 +1,9 @@
+
+
+float calculate_add(float num1, float num2);
+
+float calculate_substract(float num1, float num2);
+
+float calculate_multiply(float num1, float num2);
+
+float calculate_divide(float num1, float num2);
