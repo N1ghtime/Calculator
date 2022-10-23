a38eadec150bccb27350faa7f3d7d15e54cc0f3a
N1ghtime <nurmi.sami1@gmail.com>
2022-10-23 15:36:17 +0300

    simple calculator

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <stdio.h>
 
 int main () 
 {
-    std::cout << "Hello world!" << std::endl;
+    char Operator;
+    float num1, num2, result = 0;
+    printf("Enter any one operator like +, -, *, / :");
+    scanf("%c", &Operator);
+    printf("Enter values of num1 and num2:");
+    scanf("%f%f", &num1, &num2);
+    switch(Operator){
+        case '+': result = num1 + num2;
+            break;
+        case '-': result = num1 - num2;
+            break;
+        case '*': result = num1 * num2;
+            break;
+        case '/': result = num1 / num2;
+            break;
+        default: printf("Invalid Operator");
+    }
+    printf("The value = %f", result);
     return 0;
 }
