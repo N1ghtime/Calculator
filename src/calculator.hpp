7cb603f418cca61246a9945a30ee6b1c4792857d
N1ghtime <nurmi.sami1@gmail.com>
2022-10-30 02:27:12 +0300

    add documentation to functions

--- src/calculator.hpp
+++ src/calculator.hpp
@@ -1,11 +1,15 @@
 
-
+// returns result of num2 added to num1
 float calculate_add(float num1, float num2);
 
+// returns result of num2 substracted from num1
 float calculate_substract(float num1, float num2);
 
+// returns result of num1 multiplied by num2
 float calculate_multiply(float num1, float num2);
 
+// returns result of num1 divided by num2
 float calculate_divide(float num1, float num2);
 
+// returns reminder from division of num1 by num2
 float calculate_modulo(int num1, int num2);
